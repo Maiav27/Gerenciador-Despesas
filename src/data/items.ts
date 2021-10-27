37ec49b5acde7af973d295ebdf88348927a126a7
Maiav27 <44206589+Maiav27@users.noreply.github.com>
2021-10-27 17:22:25 -0300

    import item

--- src/data/items.ts
+++ src/data/items.ts
@@ -1,7 +1,10 @@
-export const item = [
+import { Item } from "../types/item" 
+
+export const item : Item[] = [
     { date : new Date(2021, 9, 15),    category : 'food',   title : 'McDonalds', value : 32.12   },
     { date : new Date(2021, 9, 16),    category : 'rent',   title : 'Aluguel Apt', value : 2300   },
     { date : new Date(2021, 9, 15),    category : 'food',   title : 'Burguer King', value : 23   },
     { date : new Date(2021, 8, 15),    category : 'salary',   title : 'McDonalds', value : 4500   },
     
 ]
+
