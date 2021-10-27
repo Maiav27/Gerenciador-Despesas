e3dd553217a4dd640adbad7bb776d6ac2c54e45d
Maiav27 <44206589+Maiav27@users.noreply.github.com>
2021-10-27 10:05:29 -0300

    adicionando data

---
+++ src/data/items.ts
@@ -0,0 +1,7 @@
+export const item = [
+    { date : new Date(2021, 9, 15),    category : 'food',   title : 'McDonalds', value : 32.12   },
+    { date : new Date(2021, 9, 16),    category : 'rent',   title : 'Aluguel Apt', value : 2300   },
+    { date : new Date(2021, 9, 15),    category : 'food',   title : 'Burguer King', value : 23   },
+    { date : new Date(2021, 8, 15),    category : 'salary',   title : 'McDonalds', value : 4500   },
+    
+]
